5e3eb146f3e493461f92a3ea7aaeeb5a879d3ab7
YunJin Gwak <yunjin8931@naver.com>
2023-08-21 20:46:10 +0900

    :sparkles: 생성 및 수정 시간 자동 저장

--- src/main/java/com/yunjin/springbootblog/SpringbootBlogApplication.java
+++ src/main/java/com/yunjin/springbootblog/SpringbootBlogApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // create_at, updated_at 자동 업데이트
 @SpringBootApplication
 public class SpringbootBlogApplication {
 
--- src/main/java/com/yunjin/springbootblog/domain/Article.java
+++ src/main/java/com/yunjin/springbootblog/domain/Article.java
@@ -5,6 +5,10 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -19,8 +23,17 @@ public class Article {
     @Column(name = "title", nullable = false)
     private String title;
 
-    @Column(name = "content", nullable = false)
+    @Column(name = "content", nullable = false, columnDefinition="TEXT")
     private String content;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt; // todo LocalDateTime 공부하기
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
     @Builder
     public Article(String title, String content) {
         this.title = title;
