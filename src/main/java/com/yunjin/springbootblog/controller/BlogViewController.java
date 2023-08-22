0bd2a0d856bf4c51dde5ae1aadc1c1d4a116c72d
YunJin Gwak <yunjin8931@naver.com>
2023-08-22 19:28:27 +0900

    ✨ 블로그 글 생성하기 기능 추가

--- src/main/java/com/yunjin/springbootblog/controller/BlogViewController.java
+++ src/main/java/com/yunjin/springbootblog/controller/BlogViewController.java
@@ -20,7 +20,7 @@ public class BlogViewController {
 
     /**
      * 블로그 글 전체보기 뷰
-     * @return 블로그 글 전체 리스트를 담은 뷰 반환
+     * @return 블로그 글 전체 리스트를 담은 모델뷰 반환
      */
     @GetMapping("/articles")
     public String findAllArticle(Model model) {
@@ -31,13 +31,13 @@ public class BlogViewController {
                 .toList();
 
         model.addAttribute("articles", allArticles);
-        return "articleList";
+        return "articles";
     }
 
     /**
      * 블로그 글 상세보기 뷰
      * @param id 글 ID
-     * @return 블로그 글 ID에 해당하는 내용을 담은 뷰 반환
+     * @return 블로그 글 ID에 해당하는 내용을 담은 모델뷰 반환
      */
     @GetMapping("/articles/{id}")
     public String findOneArticle(@PathVariable Long id, Model model) {
@@ -52,13 +52,22 @@ public class BlogViewController {
     /**
      * 블로그 글 수정하기 뷰
      * @param id 글 ID
-     * @return 블로그 글 ID에 해당하는 수정 대상 내용을 담은 뷰 반환
+     * @return 블로그 글 ID에 해당하는 수정 대상 내용을 담은 모델뷰 반환
      */
-    @GetMapping("/articles/edit/{id}")
-    public String newArticle(@PathVariable Long id, Model model) {
+    @GetMapping("/articles/edit/{id}") // todo URL 설계 확인
+    public String updateOneArticle(@PathVariable Long id, Model model) {
         Article article = blogService.findById(id);
         model.addAttribute("article", new ArticleViewResponseDto(article));
 
-        return "articleUpdate";
+        return "editArticle";
+    }
+
+    /**
+     * 블로그 글 생성하기 뷰
+     * @return 블로그 글 생성하기 뷰 반환
+     */
+    @GetMapping("/newArticle")
+    public String newArticle() {
+        return "newArticle";
     }
 }
