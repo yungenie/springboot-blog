bd40a7249501e6a8c5986247353782f4cff54970
YunJin Gwak <yunjin8931@naver.com>
2023-08-30 12:08:56 +0900

    🐛 createdAt, updatedAt 자동 업데이트 처리

--- src/main/java/com/yunjin/springbootblog/domain/Article.java
+++ src/main/java/com/yunjin/springbootblog/domain/Article.java
@@ -7,11 +7,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name= "article")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // Class 'Article' should have [public, protected] no-arg constructor
--- src/main/java/com/yunjin/springbootblog/domain/User.java
+++ src/main/java/com/yunjin/springbootblog/domain/User.java
@@ -5,13 +5,19 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -27,6 +33,14 @@ public class User implements UserDetails {
 
     @Column (name = "password", nullable = false)
     private String password;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     @Builder
     public User(String email, String password) {
